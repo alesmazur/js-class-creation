97b76c787a3f39c49deabc54c5253553b8c54c35
Ales Mazur <alesmazurphotography@gmail.com>
2022-08-31 20:16:19 +0200

    second comment

--- index.js
+++ index.js
@@ -7,8 +7,25 @@ class Guitar {
   upQty() {
     this.quantity += 1;
   }
+  downQty() {
+    this.quantity -= 1;
+  }
 }
 
 const newGuitar = new Guitar("Fender", "Jaguar");
 
 console.table(newGuitar);
+
+console.log(newGuitar instanceof Object);
+console.log(newGuitar instanceof Array);
+console.log(newGuitar instanceof Guitar);
+
+newGuitar.upQty();
+console.table(newGuitar);
+newGuitar.upQty();
+console.table(newGuitar);
+newGuitar.downQty();
+console.table(newGuitar);
+
+console.log(Guitar.prototype.constructor);
+console.log(newGuitar.hasOwnProperty("brand"));
